58e856bda596119bf7811d0a68f2597b86656767
xhallehaya00 <146316713+xhallehaya00@users.noreply.github.com>
2023-12-11 11:51:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showContacts() {
+  var x = document.getElementById("contactDetails");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } 
+  else {
+    x.style.display = "none";
+  }
+}
